390bf88b15986e596e474f0f94218deb43d3bf12
oeo30 <ny.tak27@gmail.com>
2025-08-25 02:38:34 +0900

    [feat] data 폴더 수정

--- data/scripts/tree_data_to_simple.py
+++ data/scripts/tree_data_to_simple.py
@@ -9,5 +9,3 @@ df_simple.to_csv('data/intermediate/street-tree-simple.csv', index=False)
 df = pd.read_csv('data/raw/ddm-park-tree.csv')
 df_simple = df[['수목명','수목위도','수목경도']]
 df_simple.to_csv('data/intermediate/park-tree-simple.csv', index=False)
-
-

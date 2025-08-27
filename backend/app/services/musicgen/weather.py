499c66be07af9eca2b12b10f66f3032e37f88ee7
oeo30 <ny.tak27@gmail.com>
2025-08-27 17:19:19 +0900

    //

--- backend/app/services/musicgen/weather.py
+++ backend/app/services/musicgen/weather.py
@@ -20,7 +20,7 @@ def get_weather(city="Seoul", country="KR"):
         raise Exception(f"Weather API error: {data}")
 
 def get_season():
-    month = datetime.now().month
+    month = datetime.now().monthㅎ
     if month in [12, 1, 2]:
         return "winter"
     elif month in [3, 4, 5]:

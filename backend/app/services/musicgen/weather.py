0b24dd40f2365d354070432d378941a0462759f2
minju <0813minju@gmail.com>
2025-08-27 15:51:35 +0900

    [feat] musicgen

--- backend/app/services/musicgen/weather.py
+++ backend/app/services/musicgen/weather.py
@@ -1,8 +1,7 @@
 import requests
 from datetime import datetime
 
-# 직접 API 키 삽입 (다른 사람도 바로 실행 가능)
-API_KEY = "a487bb124540355529986f5500bada08"
+API_KEY = "여기에 키값"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def get_weather(city="Seoul", country="KR"):
